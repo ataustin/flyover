f4b4a65c6db5f349588da446d1159c5d8aa9bb69
Adam Austin <ataustin@gmail.com>
2020-09-26 23:16:53 -0500

    update supporting functions for clearer operation

--- R/check-elements-inherit-df.R
+++ R/check-elements-inherit-df.R
@@ -1,7 +1,7 @@
 check_elements_inherit_df <- function(data_list) {
   inherits_df <- vapply(data_list, inherits, logical(1), what = "data.frame")
   if(any(!inherits_df)) {
-    stop("List objects must inherit from class data.frame
+    stop("Tabular data must inherit from class data.frame
           (e.g. data.frame, tibble, or data.table).")
   }
 }
--- R/keep-cols-by-type.R
+++ R/keep-cols-by-type.R
@@ -1,18 +1,14 @@
-keep_cols_by_type <- function(tb, type) {
-  id_col <- tb$flyover_id_
-  tb$flyover_id_ <- NULL
-  
+keep_cols_by_type <- function(tbl, type) {
   keep_fun <- switch(type,
                      numeric = is.numeric,
                      categorical = function(x) is.character(x) | is.factor(x) | is.logical(x))
   
-  tb <- tb[vapply(tb, keep_fun, logical(1))]
+  tbl <- tbl[vapply(tbl, keep_fun, logical(1))]
   
-  if(!length(tb)) {
+  if(!length(tbl)) {
     stop(paste("In keep_cols_by_type, no columns of type", type),
          call. = FALSE)
   }
   
-  tb$flyover_id_ <- id_col
-  tb
+  tbl
 }
