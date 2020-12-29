6f042483de0ee74c7022e59b04c8cdbd1cd3f452
Adam Austin <ataustin@gmail.com>
2020-12-29 17:20:56 -0600

    update enlist_data to default to names of objects passed; test and update docs

--- R/enlist-data.R
+++ R/enlist-data.R
@@ -10,23 +10,30 @@
 #' 
 #' 
 #' @param ... Comma-separated input of tabular-type data.  Any class
-#'            that inherits from \code{data.frame} is permitted.
-#' @param names Character vector of names corresponding to each data
+#'            that inherits from \code{data.frame} is permitted.  Group names
+#'            are taken from these objects, unless \code{names} is specified
+#' @param names Optional character vector of names corresponding to each data
 #'              data element in \code{...}
 #' @return A named list of \code{tibble}s
 #' @examples
 #' x <- data.frame(a = 1:3, b = letters[1:3])
 #' y <- data.frame(a = 4:6, b = letters[4:6])
+#'
+#' # groups will be named "x" and "y"
+#' enlist_data(x, y)
+#' 
+#' # groups will be named "old" and "new"
 #' enlist_data(x, y, names = c("old", "new"))
 #' 
 #' @export
 
-enlist_data <- function(..., names) {
+enlist_data <- function(..., names = NULL) {
   data_list <- list(...)
-  
   check_elements_inherit_df(data_list)
-  check_lengths(data_list, names)
   
+  if(is.null(names)) names <- vapply(match.call()[-1], deparse, character(1))
+  check_lengths(data_list, names)
+
   data_list[] <- lapply(data_list, tibble::as_tibble)
   list_out <- stats::setNames(data_list, names)
 
--- tests/testthat/test-enlist-data.R
+++ tests/testthat/test-enlist-data.R
@@ -5,17 +5,24 @@ data2   <- tibble::tibble(a = 5:8)
 data3   <- tibble::tibble(a = 9:12)
 not_tab <- letters
 
-test_that("enlist_data creates expected list", {
-  target <- list(one = data1, two = data2, three = data3)
-  expect_equivalent(enlist_data(data1,
-                                as.data.frame(data2),
-                                data.table::as.data.table(data3),
-                                names = c("one", "two", "three")),
-               target)
+test_that("enlist_data creates expected list using names", {
+  target    <- list(one = data1, two = data2, three = data3)
+  data_list <- enlist_data(data1,
+                           as.data.frame(data2),
+                           data.table::as.data.table(data3),
+                           names = c("one", "two", "three"))
+  expect_equivalent(data_list, target)             # ignore attributes
+  expect_equal(names(data_list), names(target))
 })
 
 
+test_that("enlist_data created expected list without names", {
+  target    <- list(data1 = data1, data2 = data2, data3 = data3)
+  data_list <- enlist_data(data1, data2, data3)
+  expect_equal(data_list, target)
+})
+
 test_that("length errors are handled", {
-  expect_error(enlist_data(data1, data2, data1, names = c("one", "two")))
+  expect_error(enlist_data(data1, data2, data3, names = c("one", "two")))
   expect_error(enlist_data(data1, data2, names = c("one", "two", "three")))
 })
